005eb510708c1a5481967a2fe20f6ad321a4ff72
xamibo <xamibose2001@gmail.com>
2025-05-22 20:59:30 +0530

    changes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['randomuser.me'], // ✅ Allow external image domain
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
